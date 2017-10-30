a08a81e6747f3fcf0441d88f3fe5b6a8baf18902
Benjamin Höglinger <nefarius@dhmx.at>
2017-10-30 20:29:18 +0100

    Hacked together benchmark in ViGEmTester.NET

--- NET/ViGEmTester.NET/Program.cs
+++ NET/ViGEmTester.NET/Program.cs
@@ -1,5 +1,11 @@
 ﻿using Nefarius.ViGEm.Client;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Threading;
+using System.Threading.Tasks;
 using Nefarius.ViGEm.Client.Targets;
 using Nefarius.ViGEm.Client.Targets.DualShock4;
 using Nefarius.ViGEm.Client.Targets.Xbox360;
@@ -31,14 +37,61 @@ namespace ViGEmTester.NET
             x360.SendReport(report);
 #endif
 
-            var ds4 = new DualShock4Controller(client);
+            Console.Title = "ViGEm Benchmark - CLOSE THIS WINDOW TO STOP!";
+            Console.WriteLine(Console.Title);
 
-            ds4.Connect();
+            var controllers = new ObservableCollection<DualShock4Controller>();
 
-            var report = new DualShock4Report();
-            report.SetButtons(DualShock4Buttons.Cross);
+            controllers.CollectionChanged += (sender, eventArgs) =>
+            {
+                switch (eventArgs.Action)
+                {
+                    case NotifyCollectionChangedAction.Add:
+                        Console.WriteLine("Added Controller");
+                        break;
+                    case NotifyCollectionChangedAction.Remove:
+                        Console.WriteLine("Removed Controller");
 
-            ds4.SendReport(report);
+                        if (((ObservableCollection<DualShock4Controller>) sender).Count == 0)
+                        {
+                            Console.WriteLine(" > Finished! Press any key to exit!");
+                        }
+
+                        break;
+                }
+            };
+
+            for (int i = 0; i < 500; i++)
+            {
+                Console.WriteLine($" > Spawning device {i}");
+
+                Task.Run(() =>
+                {
+                    var ds4 = new DualShock4Controller(client);
+
+                    try
+                    {
+                        lock (controllers)
+                            controllers.Add(ds4);
+
+                        ds4.Connect();
+
+                        var report = new DualShock4Report();
+                        report.SetButtons(DualShock4Buttons.Cross);
+                        ds4.SendReport(report);
+
+                        Thread.Sleep(1000);
+                        ds4.Disconnect();
+                    }
+                    finally
+                    {
+                        lock (controllers)
+                            controllers.Remove(ds4);
+                    }
+                });
+
+                Thread.Sleep(20);
+            }
 
             /*
             var x360 = new Xbox360Controller();
